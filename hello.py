98894395449e490482aa3fb7606d75ce394e6215
xiochiqui <oixelrae@gmail.com>
2022-12-18 10:55:25 -0700

    added if statements

--- hello.py
+++ hello.py
@@ -5,8 +5,8 @@ print("Hello friend!")
 ## store the person's response in FEELING
 FEELING = input("How are you today?")
 
-## include their FEELING in the print statement
-print("Oh, you're feeling " + FEELING + "  huh?")
-
-print ("if your feeling good then keep it this way if your feeling bad i hope you will feel better.")
+if FEELING == "good":
+    print ("I'm glad you are feeling good.")
+elif FEELING == "bad":
+    print ("I hope your day gets better")
 
