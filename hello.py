7b199bc85d726c29461a91b770b308671da84d18
xiochiqui <oixelrae@gmail.com>
2022-12-17 22:15:13 -0700

    trying new prints

--- hello.py
+++ hello.py
@@ -7,3 +7,6 @@ FEELING = input("How are you today?")
 
 ## include their FEELING in the print statement
 print("Oh, you're feeling " + FEELING + "  huh?")
+
+print ("if your feeling good then keep it this way if your feeling bad i hope you will feel better.")
+
