2fae759781cdf11b8fb03831bfe735df049a4ed6
xiochiqui <oixelrae@gmail.com>
2022-12-17 21:58:48 -0700

    initial commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+print("Hello friend!")
+
+## store the person's response in FEELING
+FEELING = input("How are you today?")
+
+## include their FEELING in the print statement
+print("Oh, you're feeling " + FEELING + "  huh?")
