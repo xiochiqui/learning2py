f4b0c633c24b0e5b7e99b80d7cf836a6e4dc739a
xiochiqui <oixelrae@gmail.com>
2022-12-18 15:40:11 -0700

    fixed a typo

--- hello.py
+++ hello.py
@@ -3,10 +3,11 @@
 print("Hello friend!")
 
 ## store the person's response in FEELING
-FEELING = input("How are you today?")
+FEELING = input("How are you today? ")
 
 if FEELING == "good":
     print ("I'm glad you are feeling good.")
 elif FEELING == "bad":
     print ("I hope your day gets better")
-
+else:
+    print ("I feel like your day should be good.")
