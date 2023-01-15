a0841b8e187b3778c13ba3c2429275a2f4b05128
xiochiqui <oixelrae@gmail.com>
2023-01-15 13:43:01 -0700

    Strawberries!

--- hello.py
+++ hello.py
@@ -11,3 +11,16 @@ elif FEELING == "bad":
     print ("I hope your day gets better")
 else:
     print ("Okay I hope your day will improve.")
+
+
+print("Hello friend")
+
+BERRY  = input ("Do you like strawberrys?" )
+if BERRY  == "yes":
+    print ("Okay thats good.")
+elif BERRY  == "no":
+    print ("That's to bad")
+else:
+    print ("OK")
+
+
